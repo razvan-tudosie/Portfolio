6534335f64ee8230319b5914a58e3869ebb4d80a
Razvan Tudosie <tudosie.razvan@gmail.com>
2023-08-27 15:12:13 +0300

    udpate assets links

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
     <header>
         <nav class="container max-w-6xl mx-auto flex justify-between items-center p-8">
             <div class="items-center flex gap-3">
-                <img class="w-16" src="images/avatar.png" alt="Razvan Tudosie" />
+                <img class="w-16" src="/images/avatar.png" alt="Razvan Tudosie" />
                 <NuxtLink class="text-md text-xl" to="/"><strong>Razvan</strong> Tudosie</NuxtLink>
             </div>
             <div class="invisible sm:visible">
