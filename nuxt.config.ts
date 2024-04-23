5caa58d4c7eff26aaacb75f7b909268905e44348
Razvan Tudosie <razvantudosie@192-168-0-104.rdsnet.ro>
2024-04-23 13:51:21 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,18 @@ export default defineNuxtConfig({
                     rel: 'stylesheet',
                     href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@100;300;400;700&display=swap'
                 }
-            ]
+            ],
+            script: [
+                {
+                  hid: "gtm",
+                  children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-NW7NCXJ4');`,
+                  type: "text/javascript",
+                },
+              ],
         }
     },
     modules: [
